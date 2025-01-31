c01b19ebe3fdba58903050fac0dbe3b2913b6544
smac330 <shakaylahmacklin330@gmail.com>
2025-01-30 22:49:51 -0500

    doc: added documentation for area of a circle

--- supplement1/supplement1.cs
+++ supplement1/supplement1.cs
@@ -9,6 +9,7 @@ public class supplement1
     /// <param name="length"> length of the rectangle</param>
     /// <param name="width">  width of the rectangle</param>
     /// <returns>Area of the rectangle</returns>
+    
     public static double AreaOfRectangle(double length, double width){
         if (length == width){
             return length * length;
@@ -17,6 +18,12 @@ public class supplement1
         }
     }
 
+    /// <summary>
+    /// returns the area of a circle given the radius 
+    /// </summary>
+    /// <param name="radius"> radius of a circle</param>
+    /// <returns> the area of a circle with a given radius</returns>
+    
     public static double AreaOfCircle(double radius){
         return double.Pow(radius, 2.0) * double.Pi;
     }
