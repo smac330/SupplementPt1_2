701fd9f631b80072026f73adbe4a0a83f9060f85
smac330 <shakaylahmacklin330@gmail.com>
2025-01-30 22:08:19 -0500

    feat: implemented same area of a rectangle for base case

--- supplement1.Tests/UnitTest1.cs
+++ supplement1.Tests/UnitTest1.cs
@@ -3,9 +3,16 @@ namespace supplement1.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void AreaOfARectangleCalculation()
+    public void AreaOfARectangleCalculationDifferent()
     {
         Assert.True(50.0 == supplement1.AreaOfRectangle(5.0, 10.0));
+
+    }
+
+     [Fact]
+    public void AreaOfARectangleCalculationSame()
+    {
+        Assert.True(25.0 == supplement1.AreaOfRectangle(5.0, 5.0));
         
     }
 }
