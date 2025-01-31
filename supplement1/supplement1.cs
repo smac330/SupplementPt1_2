31a0f70aa7fef36007a2aaa8e687ad33938c16e5
smac330 <shakaylahmacklin330@gmail.com>
2025-01-30 21:37:23 -0500

    chore: declared methods for calculations

--- supplement1/supplement1.cs
+++ supplement1/supplement1.cs
@@ -2,5 +2,18 @@
 
 public class supplement1
 {
+    public static double AreaOfRectangle(double length, double width){
+        throw new NotImplementedException();
 
+    }
+
+    public static double AreaOfCircle(double radius){
+        throw new NotImplementedException();
+        
+    }
+
+    public static double AreaOfTriangle(double triangleBase, double triangleHeight){
+        throw new NotImplementedException();
+        
+    }
 }
