f529695a7a6f74b2530485da7b4333e80a2bb1eb
smac330 <shakaylahmacklin330@gmail.com>
2025-01-30 22:14:24 -0500

    feat: implemented area of a rectangle for same length and width case

--- supplement1/supplement1.cs
+++ supplement1/supplement1.cs
@@ -3,7 +3,11 @@
 public class supplement1
 {
     public static double AreaOfRectangle(double length, double width){
-        return length * width;
+        if (length == width){
+            return length * length;
+        } else {
+            return length * width;
+        }
     }
 
     public static double AreaOfCircle(double radius){
