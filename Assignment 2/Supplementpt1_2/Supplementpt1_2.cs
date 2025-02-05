f184b5342b20ca056feff6a4b0a8ea037979ac9a
smac330 <shakaylahmacklin330@gmail.com>
2025-02-04 23:35:08 -0500

    final: cleaned up code and fixed any typos

--- Assignment 2/Supplementpt1_2/Supplementpt1_2.cs
+++ Assignment 2/Supplementpt1_2/Supplementpt1_2.cs
@@ -12,9 +12,8 @@ public class Supplementpt1_2
     /// </summary>
     /// <param name="mean">the mean value.</param>
     /// <param name="standardDev">the standard deviation</param>
-    /// <returns></returns>
-   public static double GenerateRandom(double mean, double standardDev)
-    {
+    /// <returns>a random number</returns>
+   public static double GenerateRandom(double mean, double standardDev){
         double num1 = 1.0 - random.NextDouble();
         double num2 = 1.0 - random.NextDouble();
         double randStandardNormal = Math.Sqrt(-2.0 * Math.Log(num1)) * Math.Sin(2.0 * Math.PI * num2);
@@ -31,15 +30,18 @@ public class Supplementpt1_2
         Random rand = new Random();
 
         return new string(Enumerable.Range(0, length).Select(_ => chars[rand.Next(chars.Length)]).ToArray());
-        } 
+    } 
 
-    public static (string, (int, int, int)) GenerateColor()
-        {
+    /// <summary>
+    /// generates a random color as a hex string and its RGB values.
+    /// </summary>
+    /// <returns> a tuple with the above values</returns>
+    public static (string, (int, int, int)) GenerateColor(){
         Random rand = new Random();
         int r = rand.Next(256);
         int g = rand.Next(256);
         int b = rand.Next(256);
         string hex = $"#{r:X2}{g:X2}{b:X2}";
         return (hex, (r, g, b));
-        } 
+    } 
 }
