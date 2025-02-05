6f73ae69715d741dabd49c7a8d6bf82f4b05ba54
smac330 <shakaylahmacklin330@gmail.com>
2025-02-04 22:33:14 -0500

    chore: implemented methods and finished test case for random number

--- Assignment 2/Supplementpt1_2.Tests/UnitTest1.cs
+++ Assignment 2/Supplementpt1_2.Tests/UnitTest1.cs
@@ -1,12 +1,29 @@
+
 namespace Supplementpt1_2.Tests;
 
 public class UnitTest1
 {
     [Fact]
+
     public void TestGenerateRandom()
     {
-        double mean = 50;
+       double mean = 50;
         double standardDev = 10;
-        double[] samples = new double[10000];
+        int sampleSize = 10000;
+        double sum = 0, sumSquares = 0;
+        Random rand = new Random();
+
+        for (int i = 0; i < sampleSize; i++)
+        {
+            double value = Supplementpt1_2.GenerateRandom(mean, standardDev);
+            sum += value;
+            sumSquares += value * value;
+        }
+
+        double calculatedMean = sum / sampleSize;
+        double calculatedStdDev = Math.Sqrt(sumSquares / sampleSize - calculatedMean * calculatedMean);
+
+        Assert.InRange(calculatedMean, mean - 1, mean + 1);
+        Assert.InRange(calculatedStdDev, standardDev - 1, standardDev + 1);
     }
 }
--- Assignment 2/Supplementpt1_2/Supplementpt1_2.cs
+++ Assignment 2/Supplementpt1_2/Supplementpt1_2.cs
@@ -2,11 +2,14 @@
 
 public class Supplementpt1_2
 {
-    private static readonly Random random = new Random();
+    public static readonly Random random = new Random();
 
    public static double GenerateRandom(double mean, double standardDev)
     {
-        throw new NotImplementedException();
+        double num1 = 1.0 - random.NextDouble();
+        double num2 = 1.0 - random.NextDouble();
+        double randStandardNormal = Math.Sqrt(-2.0 * Math.Log(num1)) * Math.Sin(2.0 * Math.PI * num2);
+        return mean + standardDev * randStandardNormal;
     } 
 
     public static double GeneratePassword(int length)
