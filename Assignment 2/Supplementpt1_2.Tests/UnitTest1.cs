78a8588126761a1ee2221da407144729002f7c01
smac330 <shakaylahmacklin330@gmail.com>
2025-02-04 21:45:38 -0500

    test: implemented test for randomg generation

--- Assignment 2/Supplementpt1_2.Tests/UnitTest1.cs
+++ Assignment 2/Supplementpt1_2.Tests/UnitTest1.cs
@@ -3,8 +3,10 @@ namespace Supplementpt1_2.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestGenerateRandom()
     {
-
+        double mean = 50;
+        double standardDev = 10;
+        double[] samples = new double[10000];
     }
 }
--- Assignment 2/Supplementpt1_2/Supplementpt1_2.cs
+++ Assignment 2/Supplementpt1_2/Supplementpt1_2.cs
@@ -1,8 +1,21 @@
 ﻿namespace Supplementpt1_2;
 
-public class Class1
+public class Supplementpt1_2
 {
     private static readonly Random random = new Random();
 
-    
+   public static double GenerateRandom(double mean, double standardDev)
+    {
+        throw new NotImplementedException();
+    } 
+
+    public static double GeneratePassword(int length)
+        {
+            throw new NotImplementedException();
+        } 
+
+    public static (string, (int, int, int)) GenerateColor()
+        {
+            throw new NotImplementedException();
+        } 
 }
